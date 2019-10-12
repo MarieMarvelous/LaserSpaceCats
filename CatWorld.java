3a803214db4b11045e77bd50a225100bba54154f
Marie Rogowski <marie.rogowski@stud.hs-hannover>
2019-10-12 12:45:41 +0200

    Changed Garden to be initalized as first level

--- CatWorld.java
+++ CatWorld.java
@@ -2,15 +2,13 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class CatWorld extends World {
 
-    public CatWorld(){    
-
-        super(1600, 900, 1); 
-        prepare();
+    public CatWorld() {
+        super(1600, 900, 1);
     }
-    
-    private void prepare()
-    {
-        CatHero catHero = new CatHero();
+
+    private void prepare() {
+        createWorld();
+        /*CatHero catHero = new CatHero();
         addObject(catHero,656,498);
         Mouse mouse = new Mouse();
         addObject(mouse,1001,426);
@@ -18,6 +16,10 @@ public class CatWorld extends World {
         catHero.setLocation(519,505);
         catHero.setLocation(667,506);
         Rat rat = new Rat();
-        addObject(rat,979,325);
+        addObject(rat,979,325);*/
+    }
+
+    public void createWorld() {
+        Greenfoot.setWorld(new Garden());
     }
 }
--- Garden.java
+++ Garden.java
@@ -6,14 +6,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Garden extends CatWorld
-{
-
-    /**
-     * Constructor for objects of class Garden.
-     * 
-     */
-    public Garden()
-    {
+public class Garden extends CatWorld {    
+    
+    public Garden(){
+        
+    }
+    
+    private void prepare() {
+        CatHero catHero = new CatHero();
+        //addObject(catHero,656,498);
+        Mouse mouse = new Mouse();
+        //Greenfoot.getWorld().addObject(mouse,1001,426);
+        mouse.setLocation(703,499);
+        catHero.setLocation(519,505);
+        catHero.setLocation(667,506);
+        Rat rat = new Rat();
+        //Greenfoot.getWorld().addObject(rat,979,325);
     }
 }
