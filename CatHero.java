c211ee810f4c93f026bd3de996e841adab58dfbf
MarieMarvelous <56111764+MarieMarvelous@users.noreply.github.com>
2019-10-23 15:17:20 +0200

    stuff

--- CatHero.java
+++ CatHero.java
@@ -2,18 +2,29 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class CatHero extends Hero {
     public static final int SPEED=5;
-    public CatHero() {
-        super(100000000);
-    }
     
+    // Hierrüber wird der Kater beim Laufen animiert
+    private GreenfootImage imagef1;
+    private GreenfootImage imagef2;    
+    private GreenfootImage imagef3;
+    private GreenfootImage imagef4;
+       
+    public CatHero() {
+        super(1);
+ 
+        imagef1 = new GreenfootImage("Cat-f-1.png"); 
+        imagef2 = new GreenfootImage("Cat-f-2.png");
+        imagef3 = new GreenfootImage("Cat-f-3.png");
+        imagef4 = new GreenfootImage("Cat-f-4.png");
+        setImage(imagef1);
+       
+    }     
     public void act() {
         super.act();
         super.movement("a", "d", "w", "s", SPEED);
         super.checkCollision();
         
-<<<<<<< HEAD
     }    
-=======
         
         if (getImage() == imagef1) {
             setImage(imagef2);
@@ -30,5 +41,20 @@ public class CatHero extends Hero {
     
    
     
->>>>>>> branch 'origin/leandra' of https://github.com/MarieMarvelous/LaserSpaceCats.git
+        
+        if (getImage() == imagef1) {
+            setImage(imagef2);
+        }
+        else {
+            if (getImage() == imagef4) {
+                setImage(imagef4);
+            }
+            else {
+                setImage(imagef4);
+            }
+        }
+    } 
+    
+   
+    
 }
