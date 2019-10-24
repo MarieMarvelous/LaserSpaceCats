89eb96167edcca2fcc2a982051320e9f99f83d5e
CrustyCraebs <55626396+CrustyCraebs@users.noreply.github.com>
2019-10-24 13:47:39 +0200

    Revert "Merge branch 'origin/leandra' into develop"
    This reverts commit e467348f2b180a5ea496e075d6f219d1caaf15f5, reversing
    changes made to f24df091dc78770f7faaf9ebb715bce1269bcf74.

--- Projectile.java
+++ Projectile.java
@@ -9,12 +9,5 @@ public abstract class Projectile extends Entity {
         if(isAtEdge()) {
             getWorld().removeObject(this);
         }
-        
-            else {  
-                if (isTouching(Artefact.class)) {
-                getWorld().removeObjects(getIntersectingObjects(Projectile.class));
-                getWorld().removeObject(this);
-            }
     }
-   }   
 }
