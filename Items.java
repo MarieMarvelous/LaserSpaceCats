aea3c45b3bbb38c7b8b02b9e56345de3fbb37247
hgok97 <56347076+hgok97@users.noreply.github.com>
2019-10-30 15:29:26 +0100

    Neue Item Klasse erstellt

---
+++ Items.java
@@ -0,0 +1,26 @@
+/**
+ * Write a description of class Items here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Items  
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Items
+     */
+    public Items()
+    {
+    }
+    
+    public void itemDrop() {
+        
+        
+        
+        
+    }
+
+}
