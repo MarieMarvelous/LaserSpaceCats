0091925599d962006874d4f656422c5c8b085b6e
MarieMarvelous <marie.rogowski@stud.hs-hannover.de>
2019-11-13 16:34:20 +0100

    added more house pieces

--- House.java
+++ House.java
@@ -10,11 +10,10 @@ public class House extends CatWorld {
     private static boolean secondWaveReady = true;
     private static boolean thirdWaveReady = true;
     private static boolean bossWaveReady = true;
-    
 
     public House() {
         super();
-        addObject(new CatHero(), 50, 50);
+        addObject(new CatHero(), 900, 400);
         spawnEnemy(EnemyType.MOUSE, 2);
         this.showText("Welle 1", 700, 25);
         killableEnemies = 7;
@@ -22,10 +21,16 @@ public class House extends CatWorld {
         addObject(new Shelf2(), 200, 30);
         addObject(new ShelfKitchen(), 260, 30);
         addObject(new Couch(), 260, 400);
+        addObject(new UpperLeftPiece(), 294, 62);
+        addObject(new UpperMiddle(), 707, 97);
+        addObject(new UpperRightPiece(), 1209, 66);
+        addObject(new Bed(), 1058, 172);
+        addObject(new Bed(), 1245, 172);
         //addHealthbar
         Healthbar healthbar = new Healthbar();
-        addObject(healthbar,275,75);
+        addObject(healthbar,225,75);
         addObject(new Glasses(), 500, 500);
+        prepare();
     }
 
     public void act() {
@@ -46,5 +51,12 @@ public class House extends CatWorld {
         checkForNextWorld(this);
     }
 
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+    }
 }
 
---
+++ UpperRightPiece.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class UpperRightPiece here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class UpperRightPiece extends Housepieces
+{
+    /**
+     * Act - do whatever the UpperRightPiece wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
