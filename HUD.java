447e03e7ab195fbfc5ad317489f35b9c1cebdbc0
Marie Rogowski <marie.rogowski@stud.hs-hannover>
2019-11-13 09:06:04 +0100

    added images to imagefolder

--- HUD.java
+++ HUD.java
@@ -6,8 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class HUD extends Entity
-{
+public class HUD extends Entity {
     /**
      * Act - do whatever the HUD wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
--- Healthbar.java
+++ Healthbar.java
@@ -6,14 +6,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Healthbar extends HUD
-{
+public class Healthbar extends HUD {
+    public Healthbar() {
+    }
     /**
      * Act - do whatever the Healthbar wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act() 
-    {
+    public void act()     {
         // Add your action code here.
     }    
 }
