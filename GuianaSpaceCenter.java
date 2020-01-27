63c953c24936971e828a58253b985cb50d9266f5
Marie Rogowski <marie.rogowski@stud.hs-hannover.de>
2020-01-27 13:39:10 +0100

    Completely reworked space station

--- GuianaSpaceCenter.java
+++ GuianaSpaceCenter.java
@@ -23,7 +23,7 @@ public class GuianaSpaceCenter extends CatWorld {
 
     public GuianaSpaceCenter() {
         super();
-        
+
         //init
         firstWaveReady = true;
         secondWaveReady = true;
@@ -35,13 +35,50 @@ public class GuianaSpaceCenter extends CatWorld {
         sixthWaveReady = true;
         eigthWaveReady = true;
         bossWaveReady = true;
-        
+
         //Sound
         SoundController.startSound("RaMxGF.mp3", true);
         addObject(WorldController.getCatHero(), 1388, 780);
 
+        addObject(new Blocking(48, 800), 24, 400);
+        addObject(new Blocking(54, 800), 1573, 400);
+        addObject(new Blocking(325, 117), 209, 58);
+        addObject(new Blocking(48, 800), 24, 400);
+        addObject(new Blocking(1017, 117), 1037, 58);
+        addObject(new Blocking(32, 32), 556, 143);
+        addObject(new Blocking(32, 32), 662, 193);
+        addObject(new Blocking(51, 96), 610, 164);
+        addObject(new Blocking(102, 100), 688, 118);
+        addObject(new Blocking(38, 61), 825, 127);
+        addObject(new Blocking(420, 47), 1218, 136);
+        addObject(new Blocking(52, 548), 1091, 377);
+        addObject(new Blocking(372, 336), 1304, 388);
+        addObject(new Blocking(479, 189), 826, 507);
+        addObject(new Blocking(53, 98), 609, 364);
+        addObject(new Blocking(1283, 97), 689, 751);
+        addObject(new Blocking(109, 99), 1493, 750);
+        addObject(new Blocking(262, 38), 828, 685);
+        addObject(new Blocking(52, 48), 611, 678);
+        addObject(new Blocking(51, 91), 770, 264);
+        addObject(new Blocking(37, 48), 1041, 388);
+        addObject(new Blocking(52, 95), 878, 264);
+        addObject(new Blocking(29, 59), 930, 218);
+        addObject(new Blocking(106, 96), 1224, 603);
+        addObject(new Blocking(106, 96), 1386, 603);
+        addObject(new Blocking(33, 43), 1465, 574);
+        addObject(new Blocking(33, 43), 1145, 574);
+        addObject(new Blocking(424, 50), 257, 678);
+        addObject(new Blocking(105, 49), 102, 630);
+        addObject(new Blocking(26, 36), 74, 580);
+        addObject(new Blocking(266, 342), 182, 286);
+        addObject(new Blocking(54, 93), 336, 409);
+        addObject(new Blocking(28, 60), 342, 265);
+        addObject(new Blocking(28, 60), 342, 123);
+        addObject(new Blocking(93, 67), 209, 513);
+        addObject(new Blocking(107, 79), 102, 500);
+
         //Leveldesign startet hier
-        addObject(new SpaceCenterBed(), 878, 264);
+        /*addObject(new SpaceCenterBed(), 878, 264);
         addObject(new SpaceCenterBed(), 292, 312);
         addObject(new SpaceCenterBed(), 184, 313);
         addObject(new SpaceCenterBed(), 290, 167);
@@ -76,7 +113,7 @@ public class GuianaSpaceCenter extends CatWorld {
         addObject(new SpaceCenterTrashcan(), 556, 145);
         addObject(new SpaceCenterTrashcan2(), 662, 191);
         addObject(new SpaceCenterUpperMiddle2a(), 556, 59);
-        addObject(new SpaceCenterTrashcan2(), 74, 579);
+        addObject(new SpaceCenterTrashcan2(), 74, 579);*/
 
         //addHealthbar
         Healthbar healthbar = new Healthbar();
